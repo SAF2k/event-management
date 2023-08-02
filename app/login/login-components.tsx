6872cd521a833f11d28a76f46b4b22bdd03b8bfa
SAFWAT <safwathkhan2@gmail.com>
2023-08-02 20:46:56 +0530

    working on login page

---
+++ app/login/login-components.tsx
@@ -0,0 +1,9 @@
+
+
+export default function LoginComponent() {
+    return (
+        <div>
+            <h1>Login Component</h1>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
               Wish to know how to host a great event?{" "}
               <a
-                href="https://newsletter.oberai.dev/p/hacking-hackathon-organizing-101"
+                href="https://github.com/SAF2k/event-management"
                 target="_blank"
                 className="font-semibold text-[#f02e65]"
               >
@@ -89,17 +89,16 @@ export default function Home() {
               <button
                 className="rounded-md bg-[#f02e65] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#990e3c] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={() => {
-                  if (localStorage.getItem("userInfo") != null) {
-                    router.push("/landing");
-                  } else {
-                    router.push("/login");
-                  }
+                  localStorage.getItem("userInfo") != null
+                    ? router.push("/landing")
+                    : router.push("/login");
                 }}
               >
                 Get started
               </button>
               <a
-                href="#"
+                target="_blank"
+                href="https://github.com/SAF2k/event-management"
                 className="text-sm font-semibold leading-6 text-gray-900"
               >
                 Learn more <span aria-hidden="true">→</span>
@@ -112,7 +111,7 @@ export default function Home() {
           aria-hidden="true"
         >
           <div
-            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#f02e65] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            className="relative left-[calc(50%+3rem)] aspect-[1255/538] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#f02e65] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
