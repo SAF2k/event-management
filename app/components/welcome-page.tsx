b1e5056e1933c6a16655a141cf842cc6f084eb57
SAFWAT <safwathkhan2@gmail.com>
2023-08-02 20:06:56 +0530

    welcome page complete

--- app/components/welcome-page.tsx
+++ app/components/welcome-page.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 
 import { useRouter } from "next/navigation";
 
 export default function WelcomeHome() {
-
-    const router = useRouter();
+  const router = useRouter();
 
   return (
     <div className="relative isolate px-6 pt-14 lg:px-8">
@@ -42,7 +41,38 @@ export default function WelcomeHome() {
             provide a great opportunity to learn and network with others in your
             field. Get started with Event-Ally to host or join your next event.
           </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <button
+              className="rounded-md bg-[#f02e65] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-[#990e3c] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              onClick={() => {
+                localStorage.getItem("userInfo") != null
+                  ? router.push("/landing")
+                  : router.push("/login");
+              }}
+            >
+              Get started
+            </button>
+            <a
+            target="_blank"
+              href="https://github.com/SAF2k/event-management"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              Learn more <span aria-hidden="true">→</span>
+            </a>
+          </div>
         </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1255/538] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#f02e65] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
       </div>
     </div>
   );
