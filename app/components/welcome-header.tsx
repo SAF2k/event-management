e86ed73b46f9c54cc74f430ae13bb45fce259cdb
SAFWAT <safwathkhan2@gmail.com>
2023-08-02 19:02:20 +0530

    working on welcome page

---
+++ app/components/welcome-header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+import Icon from "/public/logo/logo-png.png";
+
+const navigation = [
+  { name: "Product", href: "#" },
+  { name: "Features", href: "#" },
+  { name: "Company", href: "#" },
+  { name: "FAQ", href: "#" },
+];
+
+export default function WelcomeHeader() {
+    return (
+        <header className="absolute z-50 inset-x-0 top-0">
+          <nav className="flex items-center justify-between p-6 lg:px-6" aria-label="Global">
+            <div className="flex lg:flex-1">
+              <Image src={Icon} alt="logo" height={200} width={200} />
+            </div>
+            <div className="flex lg:hidden">
+                <button
+                    type="button"
+                    className="p-2.5 rounded-md inline-flex items-center justify-center text-gray-700"
+                >
+
+                </button>
+            </div>
+          </nav>
+        </header>
+    );
+}
