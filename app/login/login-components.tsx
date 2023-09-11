a23baad23510cfb2809901d80646d17a2e0527ab
SAF2k <safwathkhan2@gmail.com>
2023-09-11 22:00:02 +0530

    login page plan

--- app/login/login-components.tsx
+++ app/login/login-components.tsx
@@ -1,9 +1,12 @@
 export default function LoginComponent() {
   return (
     <div>
-      <form action="">
-        <h1>Login Component</h1>
-      </form>
+     {/* Form
+     Label 
+     Email
+      Password
+      Submit 
+      Signup Page link */}
     </div>
   );
 }
